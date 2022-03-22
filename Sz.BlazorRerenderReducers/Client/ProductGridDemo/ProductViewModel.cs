db1860a63cd1d24cc33f1ca7c28c1d7717b4caeb
Patrick Szalapski <pszalapski@gmail.com>
2022-03-22 15:35:41 -0500

    building out product grid demo page

---
+++ Sz.BlazorRerenderReducers/Client/ProductGridDemo/ProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace Sz.BlazorRerenderReducers.Client.ProductGridDemo
+{
+    public class ProductViewModel
+    {
+        public ProductViewModel()
+        {
+            this.Code = Guid.NewGuid().ToString()[..6];
+            this.Price = new Random().Next(100, 999) / 100m;
+        }
+        public string Code { get; set; }
+        public decimal Price { get; set; }
+        public bool IsInStock { get; set; } = true;
+    }
+}
