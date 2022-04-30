c13d625e1e9edf040e4b58a70cebb1a0feb0cb9a
Patrick Szalapski <pszalapski@gmail.com>
2022-04-30 15:47:08 -0500

    .

--- Sz.BlazorRerenderReducers/Client/ProductGridDemo/ProductViewModel.cs
+++ Sz.BlazorRerenderReducers/Client/ProductGridDemo/ProductViewModel.cs
@@ -1,17 +1,25 @@
 ﻿
 using System;
+using System.Collections.Generic;
+using static Sz.BlazorRerenderReducers.Client.Shared.Helpers;
 
-namespace Sz.BlazorRerenderReducers.Client.ProductGridDemo
+namespace Sz.BlazorRerenderReducers.Client.ProductGridDemo;
+
+public class ProductViewModel
 {
-    public class ProductViewModel
+    public ProductViewModel()
     {
-        public ProductViewModel()
-        {
-            this.Code = Guid.NewGuid().ToString()[..6];
-            this.Price = new Random().Next(100, 999) / 100m;
-        }
-        public string Code { get; set; }
-        public decimal Price { get; set; }
-        public bool IsInStock { get; set; } = true;
+        this.Code = Guid.NewGuid().ToString()[..6];
+        this.Price = new Random().Next(100, 999) / 100m;
+        OtherProperties.Add("MSRP", (this.Price * 1.2m).ToString());
+        OtherProperties.Add("Color", RandomOf("Red", "Green", "Blue", "White", "Gray", "Black"));
+        OtherProperties.Add("Size", RandomOf("S", "M", "L", "XL"));
+        OtherProperties.Add("Release", new DateOnly(2022, RandomOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), 1).ToString());
+        OtherProperties.Add("OS Ver", RandomOf(1, 2, 3).ToString());
+        OtherProperties.Add("Resolution", RandomOf("1600x1200", "1920x1080", "2560x1440"));
     }
+    public string Code { get; set; }
+    public decimal Price { get; set; }
+    public bool IsInStock { get; set; } = true;
+    public Dictionary<string, string> OtherProperties { get; } = new();
 }
---
+++ Sz.BlazorRerenderReducers/Client/Shared/Helpers.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Sz.BlazorRerenderReducers.Client.Shared;
+
+public static class Helpers
+{
+    public static T RandomOf<T>(params T[] items) => items[Random.Shared.Next(items.Length)];
+}
