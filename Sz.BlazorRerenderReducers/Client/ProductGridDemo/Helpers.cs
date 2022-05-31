034c6e7ad453f379835b7613999d6e169c7486b2
Patrick Szalapski <pszalapski@gmail.com>
2022-05-30 20:18:10 -0500

    .

---
+++ Sz.BlazorRerenderReducers/Client/ProductGridDemo/Helpers.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+
+namespace Sz.BlazorRerenderReducers.Client.ProductGridDemo;
+
+public static class Helpers
+{
+    // simulate an operation that takes a few ms to complete
+    public static void IntensiveOperation()
+    {
+        Thread.Sleep(2); // milliseconds
+    }
+}
