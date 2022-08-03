8eb9970d6a7e7c8943dc8189dd8371ec8b31722a
Patrick Szalapski <pszalapski@gmail.com>
2022-08-02 20:55:14 -0500

    after MDC

---
+++ Sz.BlazorRerenderReducers/Client/ProductGridDemo/ProfiledRerenderComponentBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sz.BlazorRerenderReducers.Client.ProductGridDemo;
+
+public class ProfiledRerenderComponentBase : ComponentBase
+{
+    protected override void OnAfterRender(bool firstRender)
+    {
+        Console.WriteLine($"Rendered {GetType().Name}");
+        base.OnAfterRender(firstRender);
+    }
+}
